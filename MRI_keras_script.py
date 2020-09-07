83b6e22d32778b985561ff003dfb0ee4a3b5c976
jthomson264 <jpt@alumni.stanford.edu>
2020-09-06 20:53:37 -0700

    Added Talos Functionality

--- MRI_keras_script.py
+++ MRI_keras_script.py
@@ -8,7 +8,7 @@ Train a Conv3d Network in Keras to Classify Tumors Classes From MRI Alone
 @author: ubuntr
 """
 
-
+import talos
 import numpy as np
 import tensorflow as tf
 import pandas as pd
@@ -18,7 +18,7 @@ from keras.losses import CategoricalCrossentropy
 from my_classes import DataGenerator
 
 # Parameters
-params = {'dim': (240, 240, 155),
+paramz = {'dim': (240, 240, 155),
           'batch_size': 4,
           'n_classes': 3,
           'n_channels': 4,
@@ -60,51 +60,67 @@ for index in range(0,len(IDs)):
 
 
 # Generators
-training_generator = DataGenerator(partition['train'], labels, **params)
-validation_generator = DataGenerator(partition['validation'], labels, **params)
+
 
 # Design model
-
-model = Sequential([
-    Conv3D(5, (3,3,3), strides=(1, 1, 1), padding='valid', input_shape = (240,240,155,4)),#, kernel_initializer='he_normal')),
-    BatchNormalization(),
-    Activation('relu'),
-    MaxPooling3D(pool_size=(2, 2, 2)),
-    Conv3D(5, (3,3,3), strides=(1, 1, 1), padding='valid'),#, kernel_initializer='he_normal')),
-    BatchNormalization(),
-    Activation('relu'),
-    MaxPooling3D(pool_size=(2, 2, 2)),
-    Conv3D(5, (3,3,3), strides=(1, 1, 1), padding='valid'),#, kernel_initializer='he_normal')),
-    BatchNormalization(),
-    Activation('relu'),
-    MaxPooling3D(pool_size=(3, 3, 3)),
-    Conv3D(5, (3,3,3), strides=(1, 1, 1), padding='valid'),#, kernel_initializer='he_normal')),
-    BatchNormalization(),
-    Activation('relu'),
-    MaxPooling3D(pool_size=(3, 3, 3)),
-    Flatten(),
-    Dense(8, activation='relu'),
-    #tf.keras.layers.Dropout(0.2),
-    Dense(3, activation = 'softmax') 
-    ])
-model.summary()
-loss_fn = CategoricalCrossentropy(from_logits=True)
-model.compile(optimizer='adam',
-              loss=loss_fn,
-              metrics=['accuracy'])
+training_generator = DataGenerator(partition['train'], labels, **paramz)
+validation_generator = DataGenerator(partition['validation'], labels, **paramz)
+def MRIClassifier(x, y, valX, valY, params):
+	training_generator = DataGenerator(partition['train'], labels, **paramz)
+	validation_generator = DataGenerator(partition['validation'], labels, **paramz)
+	model = Sequential([
+	    Conv3D(params['first'], (3,3,3), strides=(1, 1, 1), padding='valid', input_shape = (240,240,155,4)),
+	    BatchNormalization(),
+	    Activation('relu'),
+	    MaxPooling3D(pool_size=(2, 2, 2)),
+	    Conv3D(params['second'], (3,3,3), strides=(1, 1, 1), padding='valid'),#, kernel_initializer='he_normal')),
+	    BatchNormalization(),
+	    Activation('relu'),
+	    MaxPooling3D(pool_size=(2, 2, 2)),
+	    Conv3D(params['third'], (3,3,3), strides=(1, 1, 1), padding='valid'),#, kernel_initializer='he_normal')),
+	    BatchNormalization(),
+	    Activation('relu'),
+	    MaxPooling3D(pool_size=(3, 3, 3)),
+	    Conv3D(params['fourth'], (3,3,3), strides=(1, 1, 1), padding='valid'),#, kernel_initializer='he_normal')),
+	    BatchNormalization(),
+	    Activation('relu'),
+	    MaxPooling3D(pool_size=(3, 3, 3)),
+	    Flatten(),
+	    Dense(params['last'], activation='relu'),
+	    #tf.keras.layers.Dropout(0.2),
+	    Dense(3, activation = 'softmax') 
+	    ])
+	model.summary()
+	loss_fn = CategoricalCrossentropy(from_logits=True)
+	model.compile(optimizer='adam',
+		      loss=loss_fn,
+		      metrics=['accuracy'])
 
 
 #model.compile()
 
-# Train model on dataset
-H = model.fit_generator(generator=training_generator,
-                    validation_data=validation_generator,
-                    #use_multiprocessing=True,
-                    #workers=6,
-                    #verbose=2,
-                    epochs = 800)
+	# Train model on dataset
+	H = model.fit_generator(generator=training_generator,
+		            validation_data=validation_generator,
+		            #use_multiprocessing=True,
+		            #workers=6,
+		            #verbose=2,
+		            epochs = 15)
+	return H, model
+p = {
+	'first' : [5, 6, 7],
+	'second' : [5, 6, 7],
+	'third' : [5, 6, 7],
+	'fourth' : [5, 6, 7],
+	'last' : [4, 8, 12,16]
+}
 
-hist_df = pd.DataFrame(H.history) 
-hist_csv_file = 'overnightHistory.csv'
+dummyX,dummyY=training_generator.__getitem__(0)
+testX,testY=validation_generator.__getitem__(0)
+
+
+t = talos.Scan(x = dummyX, y=dummyY, x_val=testX, y_val=testY, model = MRIClassifier, params = p, experiment_name = 'MRI3D')
+hist_df = pd.DataFrame(t) 
+hist_csv_file = 'talosHistory.csv'
 with open(hist_csv_file, mode='w') as f:
     hist_df.to_csv(f)
